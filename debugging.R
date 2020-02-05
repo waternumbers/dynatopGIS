4ab3f0a3c3bae760c23b1d41a144a7d364ba3f21
Paul Smith <paul@waternumbers.co.uk>
2020-02-05 10:14:15 +0000

    altered debuggin script

--- debugging.R
+++ debugging.R
@@ -4,6 +4,9 @@ devtools::load_all(pacPath)
 stck <- raster::stack("debugging.gri")
 chn <- readRDS("chn_debug.rds")
 model <- create_model(stck,chn,'atb_split')
+saveRDS(model,"model_debug.rds")
+tmp <- band_model(model,10)
+saveRDS(tmp,"band_model_debug.rds")
 
 #library.dynam.unload("dynatopGIS", libpath = file.path(pacPath,"src"))
 
