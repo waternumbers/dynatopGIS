4717e255057ce6396cf69134d0115aa475e0b0b4
Paul Smith <paul@waternumbers.co.uk>
2022-09-06 17:30:30 +0100

    Removing explicit references to rgdal and rgeoss (#31)
    * Removing explicit references to rgdal and rgeoss
    
    * Add rgdal to imports until raster upgrade

--- R/dynatopGIS-package.R
+++ R/dynatopGIS-package.R
@@ -5,7 +5,6 @@
 #' @name dynatopGIS
 #' @docType package
 #' @import raster
-#' @importFrom rgeos gBuffer
 #' @importFrom jsonlite fromJSON toJSON
 #' @importFrom R6 R6Class
 #' @importFrom methods is 
